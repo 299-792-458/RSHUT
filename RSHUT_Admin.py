ab0182e3bec87667bd80e459c74ff9b82b02b858
Andrey_David <andrey.david.qwerty@gmail.com>
2024-03-11 23:13:24 +0300

    v1.8
    - Добавлен GUI
    - Исправлена ошибка при пустом вводе
    - Изменён порядок чтения из файла
    - Добавлен зайчик

--- RSHUT_Admin.py
+++ RSHUT_Admin.py
@@ -2,8 +2,9 @@ import socket
 import os
 import threading
 import time
+import tkinter as tk
 
-version = ['v1.7.1', '07.03.24']
+version = ['v1.8', '11.03.24']
 
 def command_help():
     print(f'''
@@ -56,10 +57,115 @@ def command_help():
     Для добавления программы в автозагрузку:
     WIN+R → shell:startup → скопируйте в открывшуюся папку файл RSHUT.EXE
         
+                (\___/)
+                (='.'=)
+                (")_(")
        
     DAVID_AG {version[1]} {version[0]}
     ''')
 
+# Чтение адресов из конфиг файла
+ip_dict = {}
+try:
+    config = open('config.txt', 'r', encoding='utf-8')
+    for line in config:
+        key = line.split()[0]
+        item = line.split()[1]
+        ip_dict[key] = item
+    config.close()
+except FileNotFoundError:
+    # Создание конфиг файла, если такого файла изначально нет
+    config = open('config.txt', 'x', encoding='utf-8')
+    config.close()
+except (IndexError, KeyError, ValueError):
+    print('\n<!> Файл config.txt заполнен неверно! Напишите Help для подсказки.')
+    print(ip_dict)
+
+def main_window():
+    window = tk.Tk()
+    center_x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2  # вычислить координату X для центрирования
+    center_y = (window.winfo_screenheight() - window.winfo_reqheight()) / 2  # вычислить координату Y для центрирования
+    window.wm_geometry("+%d+%d" % ((center_x - 410), (center_y - 365)))  # задать позицию окна по центру
+
+    window.title("RSHUT " + version[0])
+
+    color_1 = "#CFD8DC"
+    color_2 = "#F5F5F5"
+    color_3 = "#131418"
+    color_4 = "#E53935"
+    i = 0
+
+    def message_window(address):
+        window = tk.Tk()
+        window.geometry("400x300")
+        window.title("ОТПРАВКА СООБЩЕНИЯ")
+
+        edit = tk.Entry(window, width=50)
+        edit.grid(row=1, column= 1, pady=[30, 0], padx=50)
+
+        button = tk.Button(window, text="ОТПРАВИТЬ",
+                           command= lambda: [client(ip_dict[address], edit.get()), window.destroy()],
+                           font=("Roboto", 12), width=15, height=3,
+                           relief=tk.FLAT, fg=color_3, bg=color_1)
+        button.grid(row=2, column= 1, pady=[30, 0])
+
+        window.mainloop()
+
+    def timer_window(address):
+        pass
+
+    for address in ip_dict:
+        if i%2 == 0:
+            color_a = color_1
+            color_b = color_2
+        else:
+            color_a = color_2
+            color_b = color_1
+
+        frame = tk.Frame(window, bg=color_a, bd=1, pady=20)
+        frame.pack(fill='x')
+
+        label = tk.Label(frame, text=address, font=("Roboto", 20), fg=color_3, bg=color_a)
+        label.place(relx=0.02)
+
+        button_shutdown = tk.Button(frame, text="ВЫКЛ.",
+                                    command=lambda address=address: client(ip_dict[address], 's', '0'),
+                                    font=("Roboto", 12), width=8, height=1,
+                                    relief=tk.FLAT, fg=color_3, bg=color_b)
+
+        button_shutdown.grid(row=i, column= 1, padx=[250, 5])
+
+        button_restart = tk.Button(frame, text="ПЕРЕЗ.",
+                                   command=lambda address=address: client(ip_dict[address], 'r', '0'),
+                                   font=("Roboto", 12), width=8, height=1,
+                                   relief=tk.FLAT, fg=color_3, bg=color_b)
+
+        button_restart.grid(row=i, column= 2, padx=[0, 5])
+
+        button_msg = tk.Button(frame, text="СООБЩ.",
+                                   command=lambda address=address: message_window(address),
+                                   font=("Roboto", 12), width=8, height=1,
+                                   relief=tk.FLAT, fg=color_3, bg=color_b)
+
+        button_msg.grid(row=i, column=3)
+
+        i += 1
+
+    frame_all = tk.Frame(window, bg=color_3, bd=1, pady=20, padx=300)
+    frame_all.pack(fill='x')
+
+    button_shutdown_all = tk.Button(frame_all, text="ВЫКЛ. ВСЕ", command=window.destroy, font=("Roboto", 12), width=8, height=1,
+                                relief=tk.FLAT, fg=color_3, bg=color_2)
+    button_shutdown_all.grid(row=1, column= 1, ipadx= 20, pady= [0, 20])
+
+    button_restart_all = tk.Button(frame_all, text="ПЕРЕЗ. ВСЕ", command=window.destroy, font=("Roboto", 12), width=8, height=1,
+                               relief=tk.FLAT, fg=color_3, bg=color_2)
+    button_restart_all.grid(row=2, column= 1, ipadx= 20)
+
+
+    window.mainloop()
+
+msg = ''
 def start():
     os.system('color c')
     print(f'RSHUT {version[0]}\nНапишите Help для вызова подсказки')
@@ -68,22 +174,6 @@ def start():
         global msg
         # Создание курсора
         print('\n>> ', end='')
-        # Чтение адресов из конфиг файла
-        ip_dict = {}
-        try:
-            config = open('config.txt', 'r', encoding='utf-8')
-            for line in config:
-                key = line.split()[0]
-                item = line.split()[1]
-                ip_dict[key] = item
-            config.close()
-        except FileNotFoundError:
-            # Создание конфиг файла, если такого файла изначально нет
-            config = open('config.txt', 'x', encoding='utf-8')
-            config.close()
-        except (IndexError, KeyError, ValueError):
-            print('\n<!> Файл config.txt заполнен неверно! Напишите Help для подсказки.')
-            print(ip_dict)
 
         # Функция разбития цикла на потоки для работы со 'все'
         def command_for_all(argument, timer='0'):
@@ -100,8 +190,12 @@ def start():
                 thread.join()
 
         # Ввод данных пользователем
-        # [адрес] [атрибут]
+        # [адрес] [атрибут] [время]
         input_data = input().split()
+
+        if input_data == []:
+            print('<‼> Вы ничего не ввели!')
+            continue
         # Выключение по умолчанию
         if len(input_data) == 1:
             input_data.append('s')
@@ -186,4 +280,5 @@ def client(ip, argument, timer='0'):
     thread2.start()
 
 if __name__ == "__main__":
-    start()
+    threading.Thread(target=start).start()
+    threading.Thread(target=main_window).start()
